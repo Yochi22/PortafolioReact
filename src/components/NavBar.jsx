c0b6b1b0a4b1258a76c0cb4bdc6e2abc9e5ff075
Yochi22 <116101499+Yochi22@users.noreply.github.com>
2022-12-21 11:16:42 -0600

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,57 @@ import React from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 
 export const NavBar = () => {
+
+const links = [
+    {
+        id: 1,
+        link: 'home'
+
+    },
+
+    {
+        id: 2,
+        link: 'about'
+
+    },
+
+    {
+        id: 3,
+        link: 'projects'
+
+    },
+
+    {
+        id: 4,
+        link: 'skills'
+
+    },
+
+    {
+        id: 5,
+        link: 'contact '
+
+    },
+
+]
+
   return (
     <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
         <div>
             <h1 className='text-5xl font-signature ml-2'>Yoshi</h1>
         </div>
+
+    <ul className='flex'>
+    {links.map(({ id, link }) => (
+        <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>
+           
+            {link}
+        </li>
+    ))}
+
+        
+    </ul>
+
     </div>
   )
 }
