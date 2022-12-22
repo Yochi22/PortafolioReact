5fc54a5d28a5b8475b831769b6c676d54bce0d34
Yochi22 <116101499+Yochi22@users.noreply.github.com>
2022-12-22 12:34:21 -0600

    creation about me

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-yellow-400">About</p>
+            </div>
+            <p className="text-xl- mt-20">Actualmente me encuentro cursando un bootcamp de Desarrollo web Full Stack, Estoy continuamente aprendiendo y adquiriendo nuevas habilidades en función a las actuales y futuras demandas y tendencias tecnológicas en el campo del diseño y desarrollo de aplicaciones web en general. Me apasiona lo que hago y es esto mismo lo que me permite seguir creciendo, mejorar mis habilidades y ganar experiencia para generar ideas nuevas y soluciones que puedan enriquecer su proyecto</p>
+                <br />
+
+        </div>
+
+    </div>
+  )
+}
